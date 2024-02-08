8df7465aa7db6a59975f34eb301e039a0e0473c0
Fredrik Meyer <hrmeyer@gmail.com>
2024-02-08 11:06:36 +0100

    Fix integration test

--- src/test/java/net/fredrikmeyer/logit/site/SiteIntegrationTest.java
+++ src/test/java/net/fredrikmeyer/logit/site/SiteIntegrationTest.java
@@ -2,38 +2,42 @@ package net.fredrikmeyer.logit.site;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 class SiteIntegrationTest {
+
     @Autowired
-    private TestRestTemplate testRestTemplate;
-    @LocalServerPort
-    private int port;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
 
     @Test
-    void newTodo() {
+    @WithMockUser(username = "user", password = "password")
+    void newTodo() throws Exception {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
         map.add("value", "my new todo");
 
-        var entity = new HttpEntity<>(map, headers);
-
-        var res = testRestTemplate.postForEntity(String.format("http://localhost:%s/todo", this.port),
-                entity, String.class);
-
-        assertTrue(res.getBody()
-                .contains("my new todo"));
+        this.mockMvc.perform(post("/todo").with(csrf())
+                        .param("value", "my new todo"))
+                .andExpect(status().isOk());
     }
 }
