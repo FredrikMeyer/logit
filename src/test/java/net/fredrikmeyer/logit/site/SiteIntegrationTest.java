dd19e05513806e0f302dfc4e6ecc6828cf2fd621
Fredrik Meyer <hrmeyer@gmail.com>
2024-02-08 13:53:33 +0100

    Fix race condition in tests

--- src/test/java/net/fredrikmeyer/logit/site/SiteIntegrationTest.java
+++ src/test/java/net/fredrikmeyer/logit/site/SiteIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -38,25 +39,30 @@ class SiteIntegrationTest {
                         .param("deadline", ""))
                 .andExpect(status().isOk());
 
-        var allTodos = todoRepository.listTodos();
+        var allTodos = todoRepository.listTodos()
+                .stream()
+                .filter(t -> Objects.equals(t.content, "my new todo"))
+                .toList();
+
         assertEquals(allTodos.size(), 1);
-        var firstTodo = allTodos.getFirst();
-        assertEquals(firstTodo.content, "my new todo");
     }
 
     @Test
     @WithMockUser(username = "user", password = "password")
     void newTodoWithDeadline() throws Exception {
         this.mockMvc.perform(post("/todo").with(csrf())
-                        .param("value", "my new todo"
+                        .param("value", "my second todo"
                         )
                         .param("deadline", "2023-12-11"))
                 .andExpect(status().isOk());
 
-        var allTodos = todoRepository.listTodos();
+        var allTodos = todoRepository.listTodos()
+                .stream()
+                .filter(t -> Objects.equals(t.content, "my second todo"))
+                .toList();
+
         assertEquals(allTodos.size(), 1);
         var firstTodo = allTodos.getFirst();
-        assertEquals(firstTodo.content, "my new todo");
-        assertEquals(firstTodo.deadline, LocalDateTime.of(2023, 12,11, 0, 0));
+        assertEquals(firstTodo.deadline, LocalDateTime.of(2023, 12, 11, 0, 0));
     }
 }
